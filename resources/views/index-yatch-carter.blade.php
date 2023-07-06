449cd492b1005766a115ce10762c134146ebd3fd
zeti <nadilanio7@gmail.com>
2023-07-06 14:22:32 +0700

    ada ngubah2 di homepage pokoknya

--- resources/views/index-yatch-carter.blade.php
+++ resources/views/index-yatch-carter.blade.php
@@ -734,8 +734,8 @@
 <!--mini banner end-->
 
 <!--about us section start-->
-<div class="banner padding-bottom padding-top parallax" style="background-image: url('yatch-carter/img/headerBG.png')">
-    <div class="overlay-white"></div>
+<!--<div class="banner padding-bottom padding-top parallax" style="background-image: url('yatch-carter/img/headerBG.png')">
+    <div class="overlay-white"></div><-->
 <section class="about-sec padding-bottom" id="about-sec">
     <div class="container">
         <div class="row">
@@ -748,8 +748,7 @@
                 <p class="text">SEAVENTURES is an all-service charter agency with a hand-picked selection of the most luxurious, experience-led yachts in Indonesia. Each trip is unique: we pride ourselves on getting to know our clients so that we can craft the perfect sailing sojourn.</p>
             </div>
 </div>
-</div>
-</div>
+
         <!-- Our Team-->
          <div class="row services-area">
            
@@ -946,9 +945,6 @@
                     <ul class="list-unstyled">
                         <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="lab la-facebook-f"></i></a></li>
                         <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="lab la-twitter"></i></a></li>
-                        <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="lab la-google"></i></a></li>
-                        <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="lab la-linkedin-in"></i></a></li>
-                        <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="lab la-instagram"></i></a></li>
                         <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="las la-envelope"></i></a></li>
                     </ul>
                 </div>
@@ -967,67 +963,7 @@
 <span class="scroll-top-arrow"><i class="fas fa-angle-up"></i></span>
 <!--Scroll Top End-->
 
-<!--Shop card window Start-->
-<section class="shop-card-window hidden" id="shop-card-window">
-    <a href="#" id="close-card-window" class="close-card-window"><i class="las la-times"></i></a>
-    <div class="shop-card-window-content">
-        <h4 class="shop-card-heading">My Bag</h4>
-        <div class="d-flex justify-content-center align-items-center">
-        <div class="mini-bag">
-            <div class="bag-item">
-                <div class="item-img">
-                    <img src="yatch-carter/img/item1.jpg">
-                </div>
-                <div class="item-details">
-                    <h4 class="item-name">Product Name</h4>
-                    <span class="item-qty">Qty: 2</span>
-                    <span class="item-price">Price: $300</span>
-                    <a href="#" class="basket"><i class="las la-trash"></i> </a>
-                </div>
-            </div>
-            <div class="bag-item">
-                <div class="item-img">
-                    <img src="yatch-carter/img/item2.jpg">
-                </div>
-                <div class="item-details">
-                    <h4 class="item-name">Product Name</h4>
-                    <span class="item-qty">Qty: 2</span>
-                    <span class="item-price">Price: $300</span>
-                    <a href="#" class="basket"><i class="las la-trash"></i> </a>
-                </div>
-            </div>
-            <div class="bag-item">
-                <div class="item-img">
-                    <img src="yatch-carter/img/item3.jpg">
-                </div>
-                <div class="item-details">
-                    <h4 class="item-name">Product Name</h4>
-                    <span class="item-qty">Qty: 2</span>
-                    <span class="item-price">Price: $300</span>
-                    <a href="#" class="basket"><i class="las la-trash"></i> </a>
-                </div>
-            </div>
-            <div class="bag-item">
-                <div class="item-img">
-                    <img src="yatch-carter/img/item4.jpg">
-                </div>
-                <div class="item-details">
-                    <h4 class="item-name">Product Name</h4>
-                    <span class="item-qty">Qty: 2</span>
-                    <span class="item-price">Price: $300</span>
-                    <a href="#" class="basket"><i class="las la-trash"></i> </a>
-                </div>
-            </div>
-        </div>
-        </div>
-        <div class="bag-btn">
-            <h4 class="total"><span>Total: </span>100$</h4>
-            <a href="#" class="btn web-dark-btn rounded-pill">View Bag </a>
-            <a href="#" class="btn web-btn rounded-pill">Checkout </a>
-        </div>
-    </div>
-</section>
-<!--Shop card window  End-->
+
 
 <!--Search modal window start-->
 <div class="search_block">
@@ -1046,28 +982,28 @@
 <script src="vendor/js/bundle.min.js"></script>
 
 <!-- Plugin Js -->
-<script src="vendor/js/jquery.appear.js"></script>
-<script src="vendor/js/jquery.fancybox.min.js"></script>
-<script src="vendor/js/owl.carousel.min.js"></script>
-<script src="vendor/js/parallaxie.min.js"></script>
+<script src="{{ asset('vendor/js/jquery.appear.js')}}"></script>
+<script src="{{ asset('vendor/js/jquery.fancybox.min.js')}}"></script>
+<script src="{{ asset('vendor/js/owl.carousel.min.js')}}"></script>
+<script src="{{ asset('vendor/js/parallaxie.min.js')}}"></script>
 <!--wow js-->
-<script src="vendor/js/wow.min.js"></script>
+<script src="{{ asset('vendor/js/wow.min.js')}}"></script>
 <!-- REVOLUTION JS FILES -->
-<script src="vendor/js/jquery.themepunch.tools.min.js"></script>
-<script src="vendor/js/jquery.themepunch.revolution.min.js"></script>
-<script src="vendor/js/jquery.cubeportfolio.min.js"></script>
+<script src="{{ asset('vendor/js/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{ asset('vendor/js/jquery.themepunch.revolution.min.js')}}"></script>
+<script src="{{ asset('vendor/js/jquery.cubeportfolio.min.js')}}"></script>
 <!-- SLIDER REVOLUTION EXTENSIONS -->
-<script src="vendor/js/extensions/revolution.extension.actions.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.carousel.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.migration.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.navigation.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.parallax.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script src="vendor/js/extensions/revolution.extension.video.min.js"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.actions.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.migration.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+<script src="{{ asset('vendor/js/extensions/revolution.extension.video.min.js')}}"></script>
 
-<script src="vendor/js/wow.min.js"></script>
+<script src="{{ asset('vendor/js/wow.min.js')}}"></script>
 <!-- google map-->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJRG4KqGVNvAPY4UcVDLcLNXMXk2ktNfY"></script>
 <script src="yatch-carter/js/map.js"></script>
