2720c13edb97e6c53eecd699ebc31b0f1b0fbabe
zeti <nadilanio7@gmail.com>
2023-07-07 19:20:05 +0700

    tambah halaman login + register

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index-yatch-carter');
+    return view('login');
 });
+
+/*Buatkan route untuk ke halaman login dan halaman lainnya */
